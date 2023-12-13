e5096b42bb573bb7867fa263ba724f79f989b773
Artyem Lavrov <artyemlavrov39@gmail.com>
2023-12-13 17:42:53 +0300

    Отправщик

--- data/data_source.py
+++ data/data_source.py
@@ -1,6 +1,3 @@
-import random
-
-
 class DataSource:
 
     async def write_json(self, path: str, data: dict):
--- data/local_data_source.py
+++ data/local_data_source.py
@@ -23,7 +23,7 @@ class LocalDataSource(DataSource):
         return list(filter(
             lambda item: item,
             os.listdir(self._path(path))
-        ))[1:]
+        ))
 
     async def generate_link(self, path: str) -> str:
         return f'https://raw.githubusercontent.com/etspring/pdd_russia/master/{path}?raw=true'
